345e7f05269541e2f10b3147c070501c5da75ec1
jinsyeon <jsyjhk2000@naver.com>
2015-07-21 10:37:53 +0900

    패키지 ㅇㅇ

---
+++ PPOOL/src/main/java/com/finalproject/service/HomeController.java
@@ -0,0 +1,23 @@
+package com.finalproject.service;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+	ModelAndView mav = new ModelAndView();
+	
+	@RequestMapping(value = "home.action", method = RequestMethod.GET)
+	public ModelAndView home() {
+		
+		mav.setViewName("index");
+		return mav;
+	}
+	
+}
