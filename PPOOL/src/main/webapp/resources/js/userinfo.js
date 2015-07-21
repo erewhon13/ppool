3ef79655adbe7f72e5cbde20fa55a5824a027297
JoHyeanJun <jhswns741@gmail.com>
2015-07-21 09:57:36 +0900

    MainPage Connect

---
+++ PPOOL/src/main/webapp/resources/js/userinfo.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#logininfo").css("display","none");
+    $("#login").click(function(){
+       $("#topbar").css("display","none");
+       $("#logininfo").css("display","block");
+    });
+});
