b37ff16ac1bb94e7d4e0b0dbde13d0afa4dd25fd
JoHyeanJun <jhswns741@gmail.com>
2015-07-21 18:19:28 +0900

    Commit

--- PPOOL/src/main/java/com/ppool/service/UserService.java
+++ PPOOL/src/main/java/com/ppool/service/UserService.java
@@ -1,12 +1,6 @@
 package com.ppool.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-
 import com.ppool.dto.User;
-import com.ppool.mapper.UserMapper;
-import com.ppool.repository.UserRepository;
 
 
 public interface UserService{
