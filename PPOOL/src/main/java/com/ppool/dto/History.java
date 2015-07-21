d76c7b55e4efa96530e104e8caefd04ab47f291e
jinsyeon <jsyjhk2000@naver.com>
2015-07-21 12:21:56 +0900

    history dto생성

---
+++ PPOOL/src/main/java/com/ppool/dto/History.java
@@ -0,0 +1,84 @@
+package com.ppool.dto;
+
+import java.util.Date;
+
+public class History {
+
+	private int historyNo;
+	private String historyTitle;
+	private Date historyPeriod;
+	private int historyStaff;
+	private String historyWork;
+	private String environment;
+	private String historyContent;
+	private Date historyRegdate;
+	private boolean historyOpened;
+	private int userNo;
+	
+	public int getHistoryNo() {
+		return historyNo;
+	}
+	public void setHistoryNo(int historyNo) {
+		this.historyNo = historyNo;
+	}
+	public String getHistoryTitle() {
+		return historyTitle;
+	}
+	public void setHistoryTitle(String historyTitle) {
+		this.historyTitle = historyTitle;
+	}
+	public Date getHistoryPeriod() {
+		return historyPeriod;
+	}
+	public void setHistoryPeriod(Date historyPeriod) {
+		this.historyPeriod = historyPeriod;
+	}
+	public int getHistoryStaff() {
+		return historyStaff;
+	}
+	public void setHistoryStaff(int historyStaff) {
+		this.historyStaff = historyStaff;
+	}
+	public String getHistoryWork() {
+		return historyWork;
+	}
+	public void setHistoryWork(String historyWork) {
+		this.historyWork = historyWork;
+	}
+	public String getEnvironment() {
+		return environment;
+	}
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	public String getHistoryContent() {
+		return historyContent;
+	}
+	public void setHistoryContent(String historyContent) {
+		this.historyContent = historyContent;
+	}
+	public Date getHistoryRegdate() {
+		return historyRegdate;
+	}
+	public void setHistoryRegdate(Date historyRegdate) {
+		this.historyRegdate = historyRegdate;
+	}
+	public boolean isHistoryOpened() {
+		return historyOpened;
+	}
+	public void setHistoryOpened(boolean historyOpened) {
+		this.historyOpened = historyOpened;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	
+	
+	
+	
+	
+	
+}
