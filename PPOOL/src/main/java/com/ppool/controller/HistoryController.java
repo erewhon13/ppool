827f5e34385bea49e3d15304828acd70d2e5a360
jinsyeon <jsyjhk2000@naver.com>
2015-07-23 10:36:18 +0900

    web.xml버전 3.1로 업

--- PPOOL/src/main/java/com/ppool/controller/HistoryController.java
+++ PPOOL/src/main/java/com/ppool/controller/HistoryController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ppool.repository.HistoryRepository;
 
@@ -20,6 +22,14 @@ public class HistoryController {
 		this.historyRepository=historyRepository;
 	}
 	
+	ModelAndView mav = new ModelAndView();
+	
+	@RequestMapping(value = "historylist.action", method = RequestMethod.GET)
+	public ModelAndView historyList() {
+		
+		mav.setViewName("history/historylist");
+		return mav;
+	}
 	
 	
 
